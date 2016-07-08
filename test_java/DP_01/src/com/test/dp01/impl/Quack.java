427b25c2e7bd53690004a483b32488e17054fa70
WOO KIM <wookim0709@gmail.com>
2016-07-08 14:07:40 -0400

    a msg

---
+++ test_java/DP_01/src/com/test/dp01/impl/Quack.java
@@ -0,0 +1,10 @@
+package com.test.dp01.impl;
+
+import com.test.dp01.QuackBehavior;
+
+public class Quack implements QuackBehavior {
+	public void quack ()
+	{
+		System.out.println ("Quack");
+	}
+}
