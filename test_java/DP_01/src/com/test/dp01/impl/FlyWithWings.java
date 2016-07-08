302fc186fcc1a802487e7a95620d2c208671b490
WOO KIM <wookim0709@gmail.com>
2016-07-08 15:19:21 -0400

    test5

---
+++ test_java/DP_01/src/com/test/dp01/impl/FlyWithWings.java
@@ -0,0 +1,12 @@
+package com.test.dp01.impl;
+
+import com.test.dp01.FlyBehavior;
+
+public class FlyWithWings implements FlyBehavior {
+
+	public void fly() {
+		// TODO Auto-generated method stub
+		System.out.println("I am flying!!");
+	}
+	
+}
