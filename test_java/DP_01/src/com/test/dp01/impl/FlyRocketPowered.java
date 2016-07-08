6cf7bcd5e565d19271319374cf56282c12bc823f
WOO KIM <wookim0709@gmail.com>
2016-07-08 15:16:20 -0400

    test4

--- test_java/DP_01/src/com/test/dp01/impl/FlyRocketPowered.java
+++
@@ -1,9 +0,0 @@
-package com.test.dp01.impl;
-
-import com.test.dp01.FlyBehavior;
-
-public class FlyRocketPowered implements FlyBehavior {
-	public void fly() {
-		System.out.println ("I am flying with a rocket!");
-	}
-}
--- test_java/DP_01/src/com/test/dp01/impl/Squeak.java
+++
@@ -1,9 +0,0 @@
-package com.test.dp01.impl;
-
-import com.test.dp01.QuackBehavior;
-
-public class Squeak implements QuackBehavior {
-	public void quack () {
-		System.out.println ("Squeak");
-	}
-}
